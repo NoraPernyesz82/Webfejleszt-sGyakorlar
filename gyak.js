126be6dfd51817db4cca2518b5c9abb319e1c7b8
NoraPernyesz82 <noxpnk@gmail.com>
2019-09-12 17:10:59 +0200

    szorzotabla

---
+++ gyak.js
@@ -0,0 +1,28 @@
+
+"use strict"
+
+
+let tabla = "<table>";
+
+for (let i = 1; i < 21; i++) {
+    tabla += "<tr>";
+    for (let j = 1; j < 21; j++) {
+        if ((i == 1) || (j == 1)) {
+            tabla += `<td style="background-color: limegreen"><b>${i*j}</b></td>`;
+        }
+        else
+        {
+            if ((i % 5== 0)||(j % 5== 0)) {
+                tabla += `<td style="background-color: darkseagreen" title="${i}*${j}">${i*j}</td>`;
+            } 
+            else{
+                tabla += `<td>${i*j}</td>`;
+            }
+        }
+       
+    }
+    tabla += "</tr>";
+}
+
+tabla +=  "</table>";
+document.getElementById('elso').innerHTML = tabla;
